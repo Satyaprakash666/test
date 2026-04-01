b113384b530d8f2b52aeee19c5d7158858acd210
Satya Prakash <165584094+Satyaprakash666@users.noreply.github.com>
2026-04-01 08:21:35 +0530

    Add files via upload

---
+++ Simple/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+# CORS(app)
+
+
+@app.route('/')
+def home():
+    return 'Welcome'
+
+@app.route('/1')
+def f1():
+    return render_template('1.html')
+
+
+@app.route('/2')
+def f2():
+    return render_template('2.html')
+
+
+@app.route('/data')
+def data():
+    return 'hii'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
